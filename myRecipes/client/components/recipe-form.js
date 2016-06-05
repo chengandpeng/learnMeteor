b20cdcc0e57e9b42253d1f0726a38669ce1f6eb5
Peng Cheng <chengandpeng@gmail.com>
2016-06-06 00:01:12 +0800

    update components

--- myRecipes/client/components/recipe-form.js
+++ myRecipes/client/components/recipe-form.js
@@ -1,9 +1,41 @@
 import React from 'react';
-import { Button, Textfield, FABButton, Card, CardTitle, CardText, CardActions, Icon } from 'react-mdl';
+import { Button, Textfield, FABButton, Card, 
+				CardTitle, CardText, CardActions, Icon,
+				IconButton } from 'react-mdl';
+import _ from 'lodash';
+
+import Ingredient from './ingredient';
 
 export default class RecipeForm extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {ingredientCount: 1}
+  }
+
+  renderIngredient() {
+  	const ingredientList = [];
+  	_.times(this.state.ingredientCount, (index) => {
+  			ingredientList.push(<Ingredient index={index} handleDelete={this.handleDelete.bind(this)}/>);
+  		});
+  	return ingredientList;
+
+  }
+
+ 	handleDelete() {
+ 		this.setState({
+ 			ingredientCount: this.state.ingredientCount-1 
+ 		});
+ 	}
+
+  handleClick() {
+  	this.setState({
+  		ingredientCount: this.state.ingredientCount+1
+  	});
+  }
+
+  handleSubmit() {
+
   }
 
   render() {
@@ -13,28 +45,22 @@ export default class RecipeForm extends React.Component {
       	<Card shadow={2} className='card-new-recipe'>
 				    <CardTitle expand className='cardTitle-new-recipe'>Add New Recipe</CardTitle>
 				    <CardText>
-						  <Textfield label="Recipe Name" floatingLabel
+						  <Textfield label="Recipe Name" floatingLabel ref='recipe-name'
 						    style={{width: '400px'}}
 							/>
 							<br />
-							<Textfield label="Description" floatingLabel
+							<Textfield label="Description" floatingLabel ref='description'
 						    style={{width: '400px'}}
 							/>
 							<br />
-
-							<Textfield label="Name" floatingLabel
-						    style={{width: '150px'}}
-							/>
-							<Textfield label="Name" floatingLabel
-						    style={{width: '150px'}}
-							/>
-							<FABButton mini colored>
-							   <Icon name="add" />
-							</FABButton>
-
+							<div>
+								<span><strong>Ingredient</strong></span>
+								{ this.renderIngredient() }
+							</div>
+							<IconButton name='add' colored onClick={this.handleClick.bind(this)}/>
 				    </CardText>
 				    <CardActions border>
-				        <Button colored>Submit</Button>
+				        <Button colored onClick={this.handleSubmit.bind(this)}>Submit</Button>
 				    </CardActions>
 				</Card>
       </div>
