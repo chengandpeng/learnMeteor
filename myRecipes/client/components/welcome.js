d06c9d84a3b86fdcd97cd604085587c5a6b7dd94
Peng Cheng <chengandpeng@gmail.com>
2016-06-13 17:46:53 +0800

    update animate order

--- myRecipes/client/components/welcome.js
+++ myRecipes/client/components/welcome.js
@@ -13,15 +13,15 @@ export default class Welcome extends React.Component {
       			<div>1</div>
       			<p>Sign In or Sign Up To Start Using <strong>MyRecipe</strong></p>
       		</li>
-      		<li className='animated bounceInLeft'>
+      		<li className='animated bounceInLeft animate2'>
       			<div>2</div>
       			<p>Add Your Favorite Recipe.</p>
       		</li>
-      		<li className='animated bounceInLeft'>
+      		<li className='animated bounceInLeft animate3'>
       			<div>3</div>
       			<p>Select Recipes For Menu.</p>
       		</li>
-      		<li className='animated bounceInLeft'>
+      		<li className='animated bounceInLeft animate4'>
       			<div>4</div>
       			<p>Check Shopping List and Enjoy Yourself.</p>
       		</li>
